838966f45d4ce3fb08df9363606e900f6af6af4e
조창래 <chochanglae1223@gmail.com>
2026-02-27 19:30:40 +0900

    [Fix] 날씨 위젯 풍향 표시 영문 → 한글 변경

--- finalProject/fronted/src/components/main/WeatherSection.jsx
+++ finalProject/fronted/src/components/main/WeatherSection.jsx
@@ -30,7 +30,7 @@ function getWeatherDisplay(sky, ptyCode) {
 }
 
 function windDirText(deg) {
-  const dirs = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW'];
+  const dirs = ['북', '북북동', '북동', '동북동', '동', '동남동', '남동', '남남동', '남', '남남서', '남서', '서남서', '서', '서북서', '북서', '북북서'];
   return dirs[Math.round(deg / 22.5) % 16];
 }
 
